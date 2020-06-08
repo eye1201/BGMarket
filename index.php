81db0fa88e1105a5bf1c02ab008943a1135c1b8d
eye1201 <45666641+eye1201@users.noreply.github.com>
2020-06-08 23:28:58 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+		    <link rel="icon" href="img\bgmarketlogo.png"/>
+		    <link rel="stylesheet" href="css\eyewebstyle.css" type="text/css"/>
+        <!--LINK redirect-->
+        <link rel="canonical" href="<span class="_34Qk2">https://eyeweb.wixsite.com/eyereviews</span>"/>
+        <title>WEB REDIRECT</title>
+    </head>
+    <body>
+
+        <?php
+            echo "<p id='texto'>¡Hola, soy un script de PHP!</p>";
+            echo "<p id='texto'>Te has confundido, redireccionando...</p>";
+        ?>
+        
+    </body>
+</html>
